cc02db6c40a21061c1ffe1fa723dbb1c2a06c45d
PancakesForRent <astenbitancur@gmail.com>
2024-07-10 16:39:57 +0800

    Add operate function for callback args from math operations

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ let divide = function(operand1, operand2) {
     return operand1 / operand2;
 };
 
+function operate(operator, operand1, operand2){
+    switch (operator) {
+        case "+":
+            return add(operand1, operand2);
+        case "-":
+            return subtract(operand1, operand2);
+        case "*":
+            return multiply(operand1, operand2);
+        case "/":
+            return divide(operand1, operand2);
+    }
+}
+
