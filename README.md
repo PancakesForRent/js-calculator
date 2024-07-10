a4bba9a3f92630ed9b2eb6a6d18989de24115d13
PancakesForRent <astenbitancur@gmail.com>
2024-07-10 16:33:00 +0800

    Initialize web resources and add basic math operations

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# js-calculator
+# JavaScript Calculator Project
+
+This is the final requirement for the completion of the Foundations Course of The Odin Project.
---
+++ script.js
@@ -0,0 +1,21 @@
+let add = function(operand1, operand2) {
+    return operand1 + operand2;
+};
+
+let subtract = function(operand1, operand2) {
+    return operand1 - operand2;
+};
+
+let multiply = function(operand1, operand2) {
+    return operand1 * operand2;
+};
+
+let divide = function(operand1, operand2) {
+    return operand1 / operand2;
+};
+
+
+console.log(add(15, 16));
+console.log(subtract(8,4));
+console.log(multiply(3,5));
+console.log(divide(12,3));
